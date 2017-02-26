a80c21cac3276f9fdcbc17c954add914e4f8009e
nightism <smy961025@yeah.net>
2017-02-27 01:03:07 +0800

    several improvements on alpha - add foreign key support in 'tasks' table - set default user as Akiyama_Mio when creating tasks - fix the corrupted search engine

--- add.php
+++ add.php
@@ -11,7 +11,10 @@
 <tr>
 <td style="background-color:#eeeeee;">
 <a href="demo.php">Search</a>
-<a href="reset.php">Reset</a>
+<a href="reset.php">Reset</a> </br>
+<button>Change User (not available)</button>
+<p>Current user: Akiyama_Mio</p>
+<p>Current user_id: 1</p>
 <form>
     <input type="text" name="taskName" placeholder="Task Name" style="width: 100px">
     <input type="number" name="taskPriority" placeholder="Priority" min="0" max="3" style="width: 100px">
@@ -19,7 +22,7 @@
 </form>
 <?php if(isset($_GET['formSubmit'])) 
 {
-    $query = "INSERT INTO tasks (task_name, task_priority) VALUES ('" . $_GET['taskName'] . "', '" . $_GET['taskPriority'] . "')";
+    $query = "INSERT INTO tasks (task_name, task_priority, owner_id) VALUES ('" . $_GET['taskName'] . "', '" . $_GET['taskPriority'] . "', '1')";
     pg_query($query) or die('Add task failed: ' . pg_last_error());
     $sqlLog = $query . "<br>";
 
--- reset.php
+++ reset.php
@@ -17,6 +17,7 @@
 </form>
 <?php if(isset($_GET['formSubmit'])) 
 {
+    /************** DROP EXISTING TABLES **************/
     $sqlLog = "<br>";
     $tableNames = array("tasks", "users");
     foreach ($tableNames as &$tableName) {
@@ -24,24 +25,44 @@
         pg_query($dropTableQuery) or die ('Drop table failed: ' . pg_last_error());
         $sqlLog .= $dropTableQuery . "<br>";
     }
-    $createTasksQuery = "CREATE TABLE tasks (task_id SERIAL PRIMARY KEY, task_name varchar(100) NOT NULL, task_priority int NOT NULL, owner_id int NOT NULL)";
-    pg_query($createTasksQuery) or die ('Create table tasks failed: ' . pg_last_error());
-    $sqlLog .= $createTasksQuery . "<br>";
-    $createUsersQuery = "CREATE TABLE users (user_id SERIAL PRIMARY KEY, user_name varchar(100) NOT NULL)";
+
+    /************** CREATE user TABLES **************/
+    $createUsersQuery = "CREATE TABLE users (user_id serial PRIMARY KEY,
+                                             user_name varchar(100) NOT NULL)";
     pg_query($createUsersQuery) or die ('Create table users failed: ' . pg_last_error());
     $sqlLog .= $createUsersQuery . "<br>";
-    $tasks = array(array('Go to work', '3', '3'), array('Study', '2', '1'), array('Attend meeting', '3', '1'), array('Visit parents', '3', '0'), array('Go surfing', '1', '2'), array('Buy tickets', '3', '1'), array('Go night running', '2', '0'));
-    foreach ($tasks as &$task) {
-        $addTaskQuery = "INSERT INTO tasks (task_name, task_priority, owner_id) VALUES ('" . $task[0] . "', '" . $task[1] . "', '" . $task[2] . "')";
-        pg_query($addTaskQuery) or die('Add task failed: ' . pg_last_error());
-        $sqlLog .= $addTaskQuery . "<br>";
-    }
+
+    /************** CREATE tasks TABLES **************/
+    $createTasksQuery = "CREATE TABLE tasks (task_id SERIAL PRIMARY KEY,
+                                             task_name varchar(100) NOT NULL,
+                                             task_priority int NOT NULL,
+                                             owner_id int REFERENCES users(user_id))";
+    pg_query($createTasksQuery) or die ('Create table tasks failed: ' . pg_last_error());
+    $sqlLog .= $createTasksQuery . "<br>";
+
+
+    /************** INSERT SAMPLE users **************/
     $users = array('Akiyama_Mio', 'Kotobuki_Tsumugi', 'Hirasawa_Yui', 'Tainaka_Ritsu', 'Nakano_Azusa');
     foreach ($users as &$user) {
         $addUserQuery = "INSERT INTO users (user_name) VALUES ('" . $user . "')";
         pg_query($addUserQuery) or die('Add user failed: ' . pg_last_error());
         $sqlLog .= $addUserQuery . "<br>";
     }
+
+    /************** INSERT SAMPLE tasks **************/
+    $tasks = array(array('Go to work', '3', '3'),
+                   array('Study', '2', '1'),
+                   array('Attend meeting', '3', '1'),
+                   array('Visit parents', '3', '5'),
+                   array('Go surfing', '1', '2'),
+                   array('Buy tickets', '3', '1'),
+                   array('Go night running', '2', '4'));
+    foreach ($tasks as &$task) {
+        $addTaskQuery = "INSERT INTO tasks (task_name, task_priority, owner_id) VALUES ('" . $task[0] . "', '" . $task[1] . "', '" . $task[2] . "')";
+        pg_query($addTaskQuery) or die('Add task failed: ' . pg_last_error());
+        $sqlLog .= $addTaskQuery . "<br>";
+    }
+
     echo "<b>SQL: </b>".$sqlLog."<br><br>";
     echo "Data Reset";
     pg_free_result($tableNames);
