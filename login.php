f8a8112a82c4a4de0d1697ae2d3caf4d9ddb63cb
Yang Zhuohan <billstark1996@gmail.com>
2017-04-03 21:37:49 +0800

    finish login functionality

--- login.php
+++ login.php
@@ -1,13 +1,84 @@
-<<!DOCTYPE html>
+<!DOCTYPE html>
 <html>
 <head>
     <title>Tasource - Making task sourcing simple</title>
     <link rel="stylesheet" type="text/css" href="../TaskSourcing/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="../TaskSourcing/css/style.css">
-    <script type="text/javascript" src="../TaskSroucing/js/jquery-3.2.0.min.js"></script>
-    <script type="text/javascript" src="../TaskSourcing/bootstrap/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="../TaskSourcing/css/login-styling.css">
+
+    <script src="../TaskSourcing/js/jquery-3.2.0.min.js"></script>
+    <script src="../TaskSourcing/bootstrap/js/bootstrap.min.js"></script>
 </head>
 <body>
+    <!-- include php -->
+    <?php include "config/db-connection.php"; ?>
 
+    <div class="main-container">
+        <div class="container">
+            <div class="wrapper">
+                <div class="col-lg-4 col-md-6 col-sm-6 col-xs-8 panel panel-default ogin-panel">
+
+                    <!-- heading -->
+                    <div class="panel-title login-panelheading">
+                        <h4>Welcome</h4>
+                        <h5 class="login-panel-subtitle">Please login here.</h5>
+                    </div>
+
+                    <!-- body -->
+                    <div class="panel-body">
+
+                        <!-- login form -->
+                        <form class="login-form">
+
+                            <!-- email -->
+                            <div class="row email-input-row">
+                                <div class="col-lg-5 col-sm-5 email-label">
+                                    <h5>Email</h5>
+                                </div>
+                                <div class="col-lg-7 col-sm-7 email-input">
+                                    <input class="form-control" name="email" type="email"></input>    
+                                </div>
+                            </div>
+
+                            <!-- password -->
+                            <div class="row password-input-row">
+                                <div class="col-lg-5 col-sm-5 password-label">
+                                    <h5>Password</h5>
+                                </div>
+                                <div class="col-lg-7 col-sm-7 password-input">
+                                    <input class="form-control" name="password" type="password"></input>
+                                </div>
+                            </div>
+
+                            <!-- login button -->
+                            <div class="row login-button-wrapper">
+                                <div class="col-lg-6 col-md-6 col-xs-12">
+                                    <button type="submit" name="login" class="btn btn-large btn-success login-button">Sign in</button>
+                                </div>
+                            </div>
+                        </form>
+
+                        <?php if(isset($_GET['login'])) {
+                            $query = "SELECT u.user_id, u.user_name
+                                      FROM users u
+                                      WHERE u.email_address = '".$_GET['email']."'
+                                        AND u.password = '".$_GET[password]."';";
+                            $result = pg_query($query) or die('Query failed: ' . pg_last_error());
+
+                            $row = pg_fetch_row($result);
+                            if ($row) {
+                                echo "login success";
+                            } else {
+                                echo "Incorrect email or password";
+                            }
+
+                            pg_free_result($result);
+                        }
+                        ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
