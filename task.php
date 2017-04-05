6d7e372dca3da0f4703f66305e4be10370fd38b1
Desperado <flamesdesperado@gmail.com>
2017-04-06 02:26:34 +0800

    task page done

--- task.php
+++ task.php
@@ -31,12 +31,19 @@ if (isset($_SESSION["user_id"])) {
             $row = pg_fetch_row($result);
             if ($row) {
             	$owner_id = $row[1];
+            	$title = $row[2];
+            	$desc = nl2br($row[3]);
+				$post_time = $row[4];
+				$start_time = $row[5];
+				$end_time = $row[6];
+				$category = pg_fetch_row(pg_query("SELECT name FROM categories WHERE id = " . $row[8] . ";"))[0];
+				$region = pg_fetch_row(pg_query("SELECT name FROM regions WHERE id = " . $row[9] . ";"))[0];
+				$salary = $row[10];
             	if ($owner_id == $user_id) {
             		// User's own task
             		echo "My Task";
             	} else {
-            		// Other user's task
-            		echo "Not My Task";
+
             	}
             } else {
                 echo "Error in fetching task";
@@ -44,5 +51,42 @@ if (isset($_SESSION["user_id"])) {
             pg_free_result($result);
     	}
     ?>
+    <br>
+	<div class="container">
+		<div class="panel panel-info">
+			<div class="panel-heading"><h3><?php echo $title;?></h3></div>
+			<div class="panel-body">
+				<h4>Description: </h4><br>
+				<p><?php echo $desc;?></p><br>
+				<h5>Category:</h5>
+				<p><?php echo $category;?></p>
+				<h5>Region:</h5>
+				<p><?php echo $region;?></p>
+				<h5>Start Time:</h5>
+				<p><?php echo $start_time;?></p>
+				<h5>End Time:</h5>
+				<p><?php echo $end_time;?></p>
+				<h5>Salary:</h5>
+				<p><?php echo $salary;?></p>
+			</div>
+			<div class="panel-footer panel-info">
+				<div class="row">
+		            <div class="col-md-6">Posted on <?php echo $post_time;?></div>
+		            <div class="col-md-6">
+		            	<form class="form-inline">
+		            		<div class="form-group">
+								<label for="bidAmount">Bid For This Task:</label>
+								<input type="number" class="form-control" id="bidAmount" name="bidAmount" min="0">
+							</div>
+							<div class="form-group">
+								<input type="submit" class="form-control" value="Bid">
+							</div>
+		            	</form>
+	            	</div>
+		        </div>
+			</div>
+		</div>
+	</div>
+
 </body>
 </html>
