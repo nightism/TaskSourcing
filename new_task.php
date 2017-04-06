069c5fe92f8d94f58a0a2fba481194238c824249
Yang Zhuohan <billstark1996@gmail.com>
2017-04-06 14:05:58 +0800

    finish the create task page

--- new_task.php
+++ new_task.php
@@ -1,3 +1,13 @@
+<?php
+// Start the session
+session_start();
+if (isset($_SESSION["user_id"])) {
+    $user_id = $_SESSION["user_id"];
+} else {
+    header("Location: login.php");
+    exit;
+}
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -64,12 +74,11 @@
                     <!-- panel body -->
                     <div class="panel-body">
                         
-
                         <!-- task name -->
                         <div class="form-group row">
                             <label class="col-sm-3 control-label">Task Name</label>
                             <div class="col-sm-6">
-                                <input type="text" class="form-control" name="task_name">
+                                <input type="text" class="form-control" name="title" required="true">
                             </div>
                         </div>
 
@@ -77,14 +86,33 @@
                         <div class="form-group row">
                             <label class="col-sm-3 control-label">Task Category</label>
                             <div class="col-sm-6">
-                                <select name="category" class="form-control">
+                                <select name="category" class="form-control" required="true">
                                     <option value="">Select Category</option>
                                     <?php
-                                        $query = "SELECT c.name FROM categories c ORDER BY c.name";
+                                        $query = "SELECT c.id, c.name FROM categories c ORDER BY c.name";
                                         $result = pg_query($query) or die('Query failed: ' . pg_last_error());
 
                                         while ($row = pg_fetch_row($result)){
-                                            echo "<option value=\"".$row[0]."\">".$row[0]."</option><br>";
+                                            echo "<option value=\"".$row[0]."\">".$row[1]."</option><br>";
+                                        }
+                                        pg_free_result($result);
+                                    ?>
+                                </select>
+                            </div>
+                        </div>
+
+                        <!-- task region -->
+                        <div class="form-group row">
+                            <label class="col-sm-3 control-label">Task Region</label>
+                            <div class="col-sm-6">
+                                <select name="region" class="form-control" required="true">
+                                    <option value="">Select Region</option>
+                                    <?php
+                                        $query = "SELECT r.id, r.name FROM regions r ORDER BY r.name";
+                                        $result = pg_query($query) or die('Query failed: ' . pg_last_error());
+
+                                        while ($row = pg_fetch_row($result)){
+                                            echo "<option value=\"".$row[0]."\">".$row[1]."</option><br>";
                                         }
                                         pg_free_result($result);
                                     ?>
@@ -96,7 +124,15 @@
                         <div class="form-group row">
                             <label class="col-sm-3 control-label">Description</label>
                             <div class="col-sm-6">
-                                <textarea name="descriptoin" class="form-control autosize" style="overflow: hidden; word-wrap: break-word; resize: horizontal; height: 56px;"></textarea>
+                                <textarea name="description" class="form-control autosize" style="overflow: hidden; word-wrap: break-word; resize: horizontal; height: 56px;" required="true"></textarea>
+                            </div>
+                        </div>
+
+                        <!-- task salary -->
+                        <div class="form-group row">
+                            <label class="col-sm-3 control-label">Salary (SGD/hour)</label>
+                            <div class="col-sm-6">
+                                <input type="number" class="form-control" name="salary" required="true">
                             </div>
                         </div>
                         
@@ -105,7 +141,7 @@
                             <label class="col-sm-3 control-label">Start Time</label>
                             <div class="col-sm-6">
                                 <div class="input-group date" id="start-datetimepicker">
-                                    <input type="text" class="form-control" name="start_time">
+                                    <input type="text" class="form-control" name="start_time" required="true">
                                     <div class="input-group-addon">
                                         <i class="glyphicon glyphicon-calendar"></i>
                                     </div>
@@ -118,18 +154,19 @@
                             <label class="col-sm-3 control-label">End Time</label>
                             <div class="col-sm-6">
                                 <div class="input-group date" id="end-datetimepicker">
-                                    <input type="text" class="form-control" name="end_time">
+                                    <input type="text" class="form-control" name="end_time" required="true">
                                     <div class="input-group-addon">
                                         <i class="glyphicon glyphicon-calendar"></i>
                                     </div>
                                 </div>                                
                             </div>
                         </div>
+
                         <div class="panel-footer">
                             <div class="row">
                                 <div class="col-sm-6 col-sm-offset-3">
                                     <div class="btn-toolbar">
-                                        <button class="btn-primary btn">Create</button>
+                                        <button type="submit" name="create" class="btn-primary btn">Create</button>
                                         <button class="btn-default btn">Cancel</button>
                                     </div>
                                 </div>
@@ -138,6 +175,27 @@
                     </div>
 
                 </form>
+                <?php if(isset($_GET['create'])) {
+                    $user_id = $_SESSION["user_id"];
+                    $title = $_GET['title'];
+                    $category = $_GET['category'];
+                    $region = $_GET['region'];
+                    $description = $_GET['description'];
+                    $salary = $_GET['salary'];
+                    $start_time = $_GET['start_time'];
+                    $end_time = $_GET['end_time'];
+                    $current_time = date('Y-m-d h:i', time());
+
+                    $query = "INSERT INTO tasks(owner, title, category, region, description, salary, start_time, end_time, post_time) 
+                              VALUES (". $user_id .", '". $title ."', ". $category .", ". $region .", '". $description ."', ". $salary .", '". $start_time ."', '". $end_time ."', '". $current_time ."')";
+
+                    $result = pg_query($query) or die('Query failed: ' . pg_last_error());
+                    pg_free_result($result);
+                    header("Location: tasklist.php");
+                    echo "<script>window.location = '/TaskSourcing/tasklist.php';</script>";
+                    exit();
+                }
+                ?>
             </div>
         </div>
     </div>
